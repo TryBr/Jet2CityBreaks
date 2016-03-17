124e0ed188cb1197409b58ddcbacbe46a6c22ab7
TryBr <avulsed.jb@gmail.com>
2016-03-18 00:05:07 +0500

    Городские события сделаны.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
-var swiper = new Swiper('.swiper-container', {
+var MainSlider = new Swiper('.swiper-main', {
     effect: "fade"
+});
+
+var CityEvents = new Swiper('.swiper-events', {
+    slidesPerView: 3,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    spaceBetween: 0,
+    slidesPerColumn: 1,
+    width: 910,
 });
