2ff72050d35f84155ad8e269030d2e0418a4874f
TryBr <Avulsed.jb@gmail.com>
2016-03-18 14:55:29 +0500

    Всё сделано. Кроме мобильной версии. Внесены корректировки.

--- js/main.js
+++ js/main.js
@@ -15,6 +15,10 @@ $( "#tabs" ).tabs();
 
 var spinner = $( "#spinner, #spinner2" ).spinner();
 
+$(function() {
+    $( ".datepicker" ).datepicker();
+});
+
 $( document ).ready(function() {
    $(".ui-spinner").find(".ui-spinner-up").empty().append("<div class='plus__icon'>+</div>");
    $(".ui-spinner").find(".ui-spinner-down").empty().append("<div class='minus__icon'>-</div>");
