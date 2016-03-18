c392b771b1d41cc032a3a6d9fd2c4d0132c21ff1
TryBr <Avulsed.jb@gmail.com>
2016-03-18 14:39:57 +0500

    Всё сделано. Кроме мобильной версии.

--- js/main.js
+++ js/main.js
@@ -10,3 +10,12 @@ var CityEvents = new Swiper('.swiper-events', {
     slidesPerColumn: 1,
     width: 910,
 });
+
+$( "#tabs" ).tabs();
+
+var spinner = $( "#spinner, #spinner2" ).spinner();
+
+$( document ).ready(function() {
+   $(".ui-spinner").find(".ui-spinner-up").empty().append("<div class='plus__icon'>+</div>");
+   $(".ui-spinner").find(".ui-spinner-down").empty().append("<div class='minus__icon'>-</div>");
+});
